c46b538782d13f44165ee6a42676009a71933e79
David Porter <dporter@seek.com.au>
2014-11-20 11:07:31 +1100

    Paths and proper screen width handling

--- xi.py
+++ xi.py
@@ -2,16 +2,29 @@
 
 #don't laugh.... I don't know python
 import os
+import re
 import sys
 import subprocess
 
+if len(sys.argv) < 1:
+	print("Usage: xi <searchterm> [path]")
+
+if len(sys.argv) >= 3: 
+    path = sys.argv[2] 
+else: 
+    path = os.getcwd()
+
 searchTerm = sys.argv[1]
-
-process = subprocess.Popen(['grep -rin '+  searchTerm + " " + os.getcwd()], shell=True, stdout=subprocess.PIPE);
-
+process = subprocess.Popen(['grep -rin '+  searchTerm + " " + path], shell=True, stdout=subprocess.PIPE);
 output = process.communicate()[0]
 
 matches = [];
+
+def getTerminalWidth():
+    process = subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE);
+    return int(re.sub('[^0-9]*', '', str(process.communicate()[0])))
+
+truncationWidth = getTerminalWidth() - 20
 
 for line in output.decode("utf-8").split("\n"):
 	if(line):
@@ -21,8 +34,12 @@ option = 0
 if output: 
 	for line in matches:
 
-		# Show each line, but chop off really long lines
-		print(str(option) + " : " + (line[:100] + (line[100:] and "...")))
+		# newlines in the middle of results are annoying
+		line = re.sub("\n", "", line);
+		line = re.sub("\r", "", line);
+
+        # Show each line, but chop off really long lines
+		print(str(option) + " : " + (line[:truncationWidth] + (line[truncationWidth- 20:] and "...")))
 		option = option + 1
 
 	selection = int(input())
@@ -38,3 +55,4 @@ if output:
 		exit(1)
 else:
 	print('nothing found')
+
